fbe178538debcab199a485be38f368f30fb5aadb
Clementine Galin <galin.clem@gmail.com>
2023-12-04 21:35:56 +1100

    Schema.sql V1 + maj du modèle E/A

--- MOCODO.md
+++ MOCODO.md
@@ -1,15 +1,18 @@
-NIVEAU_LANGUES: niveau
-LANGUES: libelle
+:
+:
+NIVEAU_LANGUE: niveau
+LANGUE: libelle_langue
+:
+:
 
-TYPE_EMPLOYEUR: employeur_type
-
+:
 SPECIFICITE: id_specificite, libelle_specificite
-EXIGE2, 0N LANGUES, 0N NIVEAU_LANGUES, 03 OFFRE: exige
+EXIGE2, 0N LANGUE, 0N NIVEAU_LANGUE, 03 OFFRE: exige
 COMMUNE: id_commune, libelle_commune
 
 ACTIVITE: id_activite, libelle_activite
-DF, 11 EMPLOYEUR, 1N TYPE_EMPLOYEUR
-ADRESSE : id_adresse, deliverypoint, complement, street, distribution, subdivision, country 
+
+ADRESSE : id_adresse, delivery_point, complement, street, distribution, subdivision, country 
 
 EMPLOI: libelle_emploi, code_rome
 DF, 11 OFFRE, 1N EMPLOI
@@ -17,34 +20,44 @@ POSSEDE5, 1N SPECIFICITE, 0N OFFRE
 DF, 11 OFFRE, 1N COMMUNE
 POSSEDE2, 1N ACTIVITE, 0N OFFRE
 DF, 11 OFFRE, 1N EMPLOYEUR
-EMPLOYEUR: employeur_nomentreprise,employeur_nompersphysique,employeur_prenompersphysique,employeur_email
+EMPLOYEUR: id_employeur, type_employeur, nom_entreprise, nom_pers_physique, prenom_pers_physique, employeur_mail
 POSSEDE4, 11 ADRESSE, 1N EMPLOYEUR
 
+:
 COMPETENCE: id_competence, libelle_competence
 NECESSITE2, 1N COMPETENCE, 0N OFFRE
-OFFRE: numero,type_contrat, created,updated,aPourvoirLe,nbPostes,accompagnement,datePublication,dateArchivage,dateMiseEnAttente,dateRejet,desQuePossible,statut,signalee,dateValidite,informationComplementaire,forServiceAccompagnement
+OFFRE: numero_offre,type_contrat, created,updated,a_pouvoir_le,nb_postes,accompagnement,date_publication,date_archivage,date_mise_en_attente,date_rejet,des_que_possible,statut,signalee,date_validite,information_complementaire,for_service_accompagnement
 NECESSITE5, 1N CONTACT, ON OFFRE
-CONTACT : id_contact, nom, prenom, telephone, mobile, fax, mail
+CONTACT : id_contact, nom, prenom, telephone, mobile, fax, contact_mail
 POSSEDE3, 11 CONTACT, 1N EMPLOYEUR
+:
 
-ZONES_DEPLACEMENT: libelle
-COUVRE, 1N ZONES_DEPLACEMENT, 0N OFFRE
-DEMANDE, 11 OFFRE, 1N NIVEAU_FORMATION: diplome,certificationLocale,formationExigee
-NECESSITE3, 1N SAVOIRS_ETRE, 0N OFFRE
-NECESSITE1, 1N CONNAISSANCES_INFO, 0N OFFRE
+ZONE_DEPLACEMENT: libelle
+COUVRE, 1N ZONE_DEPLACEMENT, 0N OFFRE
+DEMANDE, 11 OFFRE, 1N NIVEAU_FORMATION: diplome,certification_locale,formation_exigee
+NECESSITE3, 1N SAVOIR_ETRE, 0N OFFRE
+NECESSITE1, 1N CONNAISSANCE_INFO, 0N OFFRE
 DF, 11 OFFRE, 1N EXPERIENCE
 EXPERIENCE: id_experience, libelle_experience
+:
 
+:
 NIVEAU_FORMATION: niveauFormation
-EXIGE1, 0N PERMIS, 03 OFFRE: exige
-SAVOIRS_ETRE: id_savoir_etre, libelle_savoir_etre
-NECESSITE4, 1N QUALIFICATIONS, 0N OFFRE
-CONNAISSANCES_INFO: id_connaisances_info, libelle_connaisances_info
+EXIGE, 0N PERMIS, 03 OFFRE: requis
+SAVOIR_ETRE: id_savoir_etre, libelle_savoir_etre
+NECESSITE4, 1N QUALIFICATION, 0N OFFRE
+CONNAISSANCE_INFO: id_connaisance_info, libelle_connaisance_info
+:
+:
 
+:
+:
 PERMIS: id_permis, libelle_permis
-
-QUALIFICATIONS: libelle
-
+:
+QUALIFICATION: libelle_qualification
+:
+:
+:
 DUREE_CONTRAT : id_duree_contrat, nombre, type_duree
 POSSEDE6, 1N DUREE_CONTRAT, 11 OFFRE
 DUREE_TEMPS_PARTIEL: id_duree_temps_partiel, nombre_heure, seuil
---
+++ schema.sql
@@ -0,0 +1,213 @@
+-- Tables simples
+
+CREATE TABLE IF NOT EXISTS activite (
+    id_activite INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_activite TEXT
+);
+
+CREATE TABLE IF NOT EXISTS commune (
+    id_commune INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_commune TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS competence (
+    id_competence INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_competence TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS connaissance_info (
+    id_connaisance_info INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_connaisance_info TEXT
+);
+
+CREATE TABLE IF NOT EXISTS zone_deplacement (
+    libelle_deplacement TEXT PRIMERY KEY
+);
+
+CREATE TABLE IF NOT EXISTS duree_contrat (
+    id_duree_contrat INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    nombre INTEGER NOT NULL,
+    type_duree TEXT CHECK(type_duree in ('an(s)','jours', 'mois'))
+);
+
+CREATE TABLE IF NOT EXISTS dureee_temps_partiel (
+    id_duree_temps_partiel INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    nombre_heure INTEGER NOT NULL,
+    seuil TEXT CHECK(seuil in ('min', 'max'))
+);
+
+CREATE TABLE IF NOT EXISTS emploi (
+    libelle_emploi TEXT PRIMARY KEY,
+    code_rome TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employeur (
+    id_employeur INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    type_employeur TEXT CHECK(type_employeur in ('ENTREPRISE', 'PARTICULIER')),
+    nom_entreprise TEXT,
+    nom_pers_physique TEXT,
+    prenom_pers_physique TEXT,
+    employeur_mail TEXT
+);
+
+CREATE TABLE IF NOT EXISTS permis (
+    id_permis INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_permis TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS langue (
+    libelle_langue TEXT PRIMARY KEY
+)
+
+CREATE TABLE IF NOT EXISTS niveau_langue (
+    libelle_niveau TEXT PRIMARY KEY
+)
+
+CREATE TABLE IF NOT EXISTS experience (
+  PRIMARY KEY (id_experience),
+  id_experience INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  libelle_experience TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS competence (
+    id_competence INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_competence TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS savoir_etre (
+    id_savoir_etre INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_savoir_etre TEXT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS qualification (
+    libelle_qualification TEXT PRIMARY KEY
+)
+
+CREATE TABLE IF NOT EXISTS niveau_formation (
+    niveau_formation TEXT PRIMARY KEY
+)
+
+CREATE TABLE IF NOT EXISTS specificite (
+    id_specificite INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    libelle_specificite TEXT NOT NULL
+)
+
+-- Tables avec une clé étrangère
+
+CREATE TABLE IF NOT EXISTS contact (
+      id_contact INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      nom TEXT NOT NULL,
+      prenom TEXT NOT NULL,
+      telephone TEXT,
+      mobile TEXT,
+      fax TEXT,
+      contact_mail TEXT,
+      id_employeur TEXT NOT NULL REFERENCES employeur(id_employeur)
+);
+
+CREATE TABLE IF NOT EXISTS adresse (
+      id_adresse INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      delivery_point TEXT,
+      complement TEXT,
+      street TEXT
+      distribution TEXT,
+      subdivision TEXT,
+      country TEXT,
+      id_employeur TEXT NOT NULL REFERENCES employeur(id_employeur)
+);
+
+
+-- Tables avec plusieurs clés étrangères
+
+CREATE TABLE IF NOT EXISTS couvrir_deplacement (
+      PRIMARY KEY (libelle_deplacement, numero_offre),
+      libelle_deplacement TEXT REFERENCES zone_deplacement(libelle_deplacement)
+      numero_offre TEXT REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS permis_requis (
+      PRIMARY KEY (id_permis, numero_offre),
+      id_permis TEXT REFERENCES permis(id_permis),
+      numero_offre TEXT REFERENCES offre(numero_offre),
+      requis TEXT NOT NULL CHECK(requis in ('True', 'False'))
+);
+
+CREATE TABLE IF NOT EXISTS langage_requis (
+      PRIMARY KEY (libelle_langue, niveau, numero_offre),
+      libelle_langue REFERENCES langue(libelle_langue),
+      niveau REFERENCES niveau_langue(libelle_niveau),
+      numero_offre  REFERENCES offre(numero_offre),
+      requis TEXT NOT NULL CHECK(requis in ('true', 'false'))
+);
+
+CREATE TABLE IF NOT EXISTS connaissance_requise (
+      PRIMARY KEY (id_connaisance_info, numero_offre),
+      id_connaisance_info INTEGER REFERENCES connaissance_info(id_connaisance_info),
+      numero_offre REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS competence_requise (
+    PRIMARY KEY (id_competence, numero_offre),
+    id_competence INTEGER REFERENCES competence(id_competence)
+    numero_offre REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS savoir_etre_requis (
+    PRIMARY KEY (id_savoir_etre, numero_offre),
+    id_savoir_etre INTEGER REFERENCES savoir_etre(id_savoir_etre),
+    numero_offre REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS qualification_requise (
+  PRIMARY KEY (libelle_qualification, numero_offre),
+    libelle_qualification TEXT REFERENCES qualification(libelle_qualification),
+    numero_offre REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS contact_offre (
+    PRIMARY KEY (id_contact, numero_offre),
+    id_contact INTEGER REFERENCES contact(id_contact),
+    numero_offre REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS activite_requise (
+  PRIMARY KEY (id_activite, numero_offre),
+  id_activite INTEGER REFERENCES activite(id_activite),
+  numero_offre TEXT REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS specificite_requise (
+  PRIMARY KEY (id_specificite, numero_offre),
+  id_specificite INTEGER REFERENCES specificite(id_specificite),
+  numero_offre TEXT REFERENCES offre(numero_offre)
+);
+
+CREATE TABLE IF NOT EXISTS offre (
+  numero_offre TEXT PRIMARY KEY,
+  type_contrat TEXT NOT NULL CHECK(type_contrat in ('CDD', 'CDI', 'CDD évolutif'))
+  created DATE NOT NULL,
+  updated DATE NOT NULL,
+  a_pouvoir_le DATE NOT NULL,
+  nb_postes INTEGER NOT NULL,
+  accompagnement INTEGER NOT NULL CHECK(accompagnement in (0, 1)),
+  date_publication DATE NOT NULL,
+  date_archivage DATE,
+  date_mise_en_attente DATE,
+  date_rejet DATE,
+  des_que_possible INTEGER NOT NULL CHECK(des_que_possible in (0, 1)),
+  statut TEXT NOT NULL,
+  signalee INTEGER NOT NULL CHECK(signalee in (0, 1)),
+  date_validite DATE,
+  information_complementaire TEXT,
+  for_service_accompagnement INTEGER NOT NULL CHECK(for_service_accompagnement in (0, 1)),
+  libelle_emploi TEXT NOT NULL REFERENCES emploi(libelle_emploi),
+  id_commune INTEGERNOT NULL REFERENCES commune(id_commune),
+  id_employeur INTEGER NOT NULL REFERENCES employeur(id_employeur),
+  niveau_Formation REFERENCES niveau_formation(niveau_formation),
+  diplome TEXT,
+  certification_locale TEXT,
+  formation_exigee INTEGER NOT NULL CHECK(formation_exigee in (0, 1)),
+  id_experience INTEGER NOT NULL REFERENCES experience(id_experience),
+  id_duree_contrat INTEGER REFERENCES duree_contrat(id_duree_contrat),
+  id_duree_temps_partiel INTEGER REFERENCES duree_temps_partiel(id_duree_temps_partiel)
+);
